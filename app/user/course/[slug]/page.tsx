d88d0e33b1ba34aab4634ffe83ab561fcecc71a2
arfat-xyz <arfatrahman08@gmail.com>
2025-04-14 16:27:17 +0600

    updated

--- app/user/course/[slug]/page.tsx
+++ app/user/course/[slug]/page.tsx
@@ -1,7 +1,17 @@
 import SingleUserCourseClientComponent from "@/components/user/course/single-course-client-component";
-import { metaDataGeneratorForNormalPage } from "@/lib/meta-generator";
+import axiosInstance from "@/lib/axios-instance";
+import { generateCourseMetadata } from "@/lib/meta-generator";
+import { Metadata } from "next";
 
-export const metadata = metaDataGeneratorForNormalPage("Become Instructor");
+export async function generateMetadata({
+  params: { slug },
+}: {
+  params: { slug: string };
+}): Promise<Metadata> {
+  const { data } = await axiosInstance.get(`/course/single-course/${slug}`);
+  return generateCourseMetadata(data?.data);
+}
+
 const SingleCOursePage = ({
   params: { slug },
 }: {
